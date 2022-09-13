e5f433dbd9f3226330cf6934a5de0d48f7ce7f7d
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-13 05:06:46 +0300

    Failed to get data because of No 'Access-Control-Allow-Origin'

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const btn_polindrome=document.getElementById('btn_polindrome');
+const text_polindrome=document.getElementById('text_polindrome');
+const text_result_polindrome=document.getElementById('text_result_polindrome');
+
+let checkPolindrome = (e) =>{
+    let url_polindrome="http://127.0.0.1:80/xampp/palindrome_string.php?string=".concat(text_polindrome.value);
+    console.log(url_polindrome)
+    fetch(url_polindrome)
+    .then((response) => response.json())
+    .then((data) => {
+    text_result_polindrome.innerText=Object.values(data)[0];
+  });
+}
+btn_polindrome.addEventListener("click",checkPolindrome);
+
+
+// Get the json request from url and fill image
+// fetch('https://dog.ceo/api/breeds/image/random')
+// .then((response) => response.json())
+// .then((data) => {
+//     // let image_content=Object.values(data)[0];
+//     // dog_image.innerHTML='<img src= "'+image_content+'"/>';
+//   });
