bbb750ad6c9ad071f9b7d5727746c69c7c6a70b5
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-13 05:37:42 +0300

    Fix some errors

--- php/calculate_numbers.php
+++ php/calculate_numbers.php
@@ -3,7 +3,10 @@
 $a=$_POST['a'];
 $b=$_POST['b'];
 $c=$_POST['c'];
-$result=($a*$a*$a)+($b*$c)-($a/$b);
+if($b!=0){
+    $result=($a*$a*$a)+($b*$c)-($a/$b);
+}
+
 // Create Json
 $return =[
     "result" => $result,
